ba03bd22081a461e2855741a3e49dd2a2fe0fec1
ycy-ycy <2923616405@qq.com>
2020-02-11 11:25:12 +0800

    Update

--- src/main/java/com/pyc/transaction/domain/Person.java
+++ src/main/java/com/pyc/transaction/domain/Person.java
@@ -7,5 +7,60 @@
 
 package com.pyc.transaction.domain;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private Integer age;
+    private String address;
+
+    public Person(){
+        super();
+    }
+    public Person(Long id,String name, Integer age, String address){
+        super();
+        this.id=id;
+        this.name=name;
+        this.age=age;
+        this.address=address;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
